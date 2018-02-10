c9f43d15caa843e5a7c4d6976721c4e0cfe9988f
zhhello <781899128@qq.com>
2018-02-10 21:58:51 +0800

    First commit

--- app/src/main/java/com/example/hui/bestui/MainActivity.java
+++ app/src/main/java/com/example/hui/bestui/MainActivity.java
@@ -1,13 +1,208 @@
 package com.example.hui.bestui;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawLayout;
+
+    private SwipeRefreshLayout swipeRefresh;
+
+    private List<Fruit>fruitList = new ArrayList<>();
+
+    private FruitAdapter adapter;
+
+    private Fruit[] fruits = {new Fruit("Apple",R.drawable.apple),new Fruit("Banana",R.drawable.banana),new Fruit("Orange",R.drawable.orange),new Fruit("Strawberry",R.drawable.strawberry)};
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        //setIconsVisible(menu, true);
+        //return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        return true;
+//        MenuInflater inflater = new MenuInflater(this);
+//        inflater.inflate(R.menu.nav_menu,menu);
+//        setIconsVisible(menu,true);  //  就是这一句使图标能显示
+//        return super.onCreateOptionsMenu(menu);
+    }
+
+
+//    private void setIconEnable(Menu menu, boolean enable)
+//    {
+//        try
+//        {
+//            Class<?> clazz = Class.forName("com.android.internal.view.menu.MenuBuilder");
+//            Method m = clazz.getDeclaredMethod("setOptionalIconsVisible", boolean.class);
+//            m.setAccessible(true);
+//
+//            //MenuBuilder实现Menu接口，创建菜单时，传进来的menu其实就是MenuBuilder对象(java的多态特征)
+//            m.invoke(menu, enable);
+//
+//        } catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
+//    }
+
+
+
+
+
+
+
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.backup:
+                Toast.makeText(this,"You clicked Backup",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.delete:
+                Toast.makeText(this,"You clicked Delete",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.settings:
+                Toast.makeText(this,"You clicked Settings",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.mipmap.ic_menu);
+        }
+        navView.setCheckedItem(R.id.nav_call);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                mDrawLayout.closeDrawers();
+                return true;
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(MainActivity.this,"FAB clicked",Toast.LENGTH_SHORT).show();
+                Snackbar.make(view, "Data deleted", Snackbar.LENGTH_SHORT).setAction("Undo", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "Data restored", Toast.LENGTH_SHORT).show();
+                    }
+                }).show();
+            }
+        });
+
+        initFruits();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new FruitAdapter(fruitList);
+        recyclerView.setAdapter(adapter);
+        swipeRefresh = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        swipeRefresh.setColorSchemeResources(R.color.colorPrimary);
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+
+
+            @Override
+            public void onRefresh() {
+                refreshFruits();
+            }
+        });
     }
+
+    private void refreshFruits(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        initFruits();
+                        adapter.notifyDataSetChanged();
+                        swipeRefresh.setRefreshing(false);
+                    }
+                });
+            }
+        }).start();
+    }
+
+    private void initFruits(){
+        fruitList.clear();
+        for(int i = 0; i < 50;i++){
+            Random random = new Random();
+            int index = random.nextInt(fruits.length);
+            fruitList.add(fruits[index]);
+        }
+    }
+
+    public boolean onOptionItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                mDrawLayout.openDrawer(GravityCompat.START);
+
+                break;
+            default:
+                break;
+
+        }
+        return true;
+    }
+
+//    private void setIconsVisible(Menu menu, boolean flag) {
+//        //判断menu是否为空
+//        if(menu != null) {
+//            try {
+//                //如果不为空,就反射拿到menu的setOptionalIconsVisible方法
+//                Method method = menu.getClass().getDeclaredMethod("setOptionalIconsVisible", Boolean.TYPE);
+//                //暴力访问该方法
+//                method.setAccessible(true);
+//                //调用该方法显示icon
+//                method.invoke(menu, flag);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 }
